7c79144c46f6e1b0a43958f65e6b7e0d2f258989
klandestyno.ogame@gmail.com <klandestyno.ogame@gmail.com>
2024-05-09 00:23:09 +0000

    first stg

---
+++ models/staging/stg_productvendor.sql
@@ -0,0 +1,4 @@
+SELECT productid, 
+businessentityid, 
+averageleadtime, standardprice, lastreceiptcost, lastreceiptdate, minorderqty, maxorderqty, onorderqty, unitmeasurecode, modifieddate
+from "advworks_famd"."purchasing"."productvendor"
