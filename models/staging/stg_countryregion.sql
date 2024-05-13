afc1d125e2403767517195e8c540b8e73ec44494
klandestyno.ogame@gmail.com <klandestyno.ogame@gmail.com>
2024-05-13 11:46:39 +0000

    changes

---
+++ models/staging/stg_countryregion.sql
@@ -0,0 +1,5 @@
+select
+    countryregioncode
+    , modifieddate
+    , name as country_name
+from "advworks_famd"."person"."countryregion"
--- models/staging/stg_productvendor.sql
+++ models/staging/stg_productvendor.sql
@@ -1,4 +1,12 @@
-SELECT productid, 
-businessentityid, 
-averageleadtime, standardprice, lastreceiptcost, lastreceiptdate, minorderqty, maxorderqty, onorderqty, unitmeasurecode, modifieddate
+SELECT productid
+    , businessentityid
+    , averageleadtime
+    , standardprice
+    , lastreceiptcost
+    , lastreceiptdate
+    , minorderqty
+    , maxorderqty
+    , onorderqty
+    , unitmeasurecode
+    , modifieddate
 from "advworks_famd"."purchasing"."productvendor"
