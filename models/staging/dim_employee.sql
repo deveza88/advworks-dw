86603af5496d8f975e950fb4c83e25d90fc54742
klandestyno.ogame@gmail.com <klandestyno.ogame@gmail.com>
2024-05-15 22:05:42 +0000

    v2

--- models/staging/dim_employee.sql
+++ models/staging/dim_employee.sql
@@ -1,8 +1,7 @@
 -- Joining stg_employee and stg_person tables
 select
     e.employee_id,
-    p.businessentity_id as person_id,
-    p.firstname,
-    p.lastname,
+    p.businessentity_id,
+    concat(p.firstname, ' ', p.lastname) as full_name
 from {{ ref('stg_employee') }} e
 left join {{ ref('stg_person') }} p on e.employee_id = p.businessentity_id
--- models/staging/stg_salesreason.sql
+++ models/staging/stg_salesreason.sql
@@ -5,6 +5,7 @@ with source_data as (
         , reasontype
         , modifieddate as last_update
     from {{ source('sales', 'salesreason') }}
+    where salesreasonid is not null -- Filtering out null values from salesreason_id
 )
 select *
 from source_data
